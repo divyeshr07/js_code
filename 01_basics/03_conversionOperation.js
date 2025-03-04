772c1170e8ff4abf00be004f4d6d09eadf3a3610
divyeshr07 <divyesh.rushkar@gmail.com>
2025-03-04 11:28:38 +0000

    conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,4 @@
+//.....conversion......
 let score = false
 
 // console.log(typeof score);
@@ -25,7 +26,41 @@ let booleanIsLoggenIn = Boolean(isLoggedIn)
 
 let someNumber = 21
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 
+//........operation........
+
+let value = 7
+let negValue = -value
+
+//console.log(negValue);
+
+//maths operation 
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%3);
+
+let str = "hello"
+let str1 = "hitesh"
+let str2 = (str+str1)
+
+// console.log(str2);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+"9"+2);
+
+let num1, num2, num3 
+
+num1 = num2 = num3 =2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
