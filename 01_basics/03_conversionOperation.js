abfb4f3fd657602bd65425d7e970283cb2f8a978
divyeshr07 <divyesh.rushkar@gmail.com>
2025-03-04 10:08:03 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = false
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" = "33"
+// "33abc" = NaN
+// true = 1 false = 0
+
+let isLoggedIn = "divyesh"
+
+let booleanIsLoggenIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggenIn);
+
+// 1 = true; 0 = false
+// "" = false
+// "divyesh" = true
+
+let someNumber = 21
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
