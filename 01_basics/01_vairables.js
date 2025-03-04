28a491cd3efd096cd5479f9a6c63556f16b588c7
divyeshr07 <divyesh.rushkar@gmail.com>
2025-03-04 07:32:42 +0000

    vairables

---
+++ 01_basics/01_vairables.js
@@ -0,0 +1,17 @@
+// vairables me tow words me second wored me fast wored capitel hota hee.
+// const me value cheng nay hoti he
+
+const accountId = 21102002
+let accountEmail =  "dnakum2110@gamil.com"
+var accountPassword = "2107"
+accountCity = "surat"
+
+// accountId = 21 note allowed 
+
+accountEmail = "divyesh2107@gmail.com"
+accountPassword = "0721"
+accountCity ="rajkot"
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
