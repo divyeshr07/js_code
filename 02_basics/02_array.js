a8e9440d803a9b9e744e241445ce31d071d77a02
divyeshr07 <divyesh.rushkar@gmail.com>
2025-03-06 12:47:15 +0000

    array2

---
+++ 02_basics/02_array.js
@@ -0,0 +1,29 @@
+
+const boys1 = ["manoj","rohan","dilip"]
+const boys2 = ["atul","vikram","ankit"]
+
+// boys1.push(boys2)
+
+// console.log(boys1);
+// console.log(boys1[3][1]);
+
+// const boys3 = boys1.concat(boys2)
+// console.log(boys3);
+
+// const boysg = [...boys1,...boys2]
+// console.log(boysg);
+
+// const boyspg = [1,2,3,[4,5],6,7,[7],8]
+// const boyspg1 = boyspg.flat(Infinity)
+// console.log(boyspg1);
+
+console.log(Array.isArray("divyesh"));
+console.log(Array.from("divyesh"));
+console.log(Array.from({name: "divyesh"}));
+
+let score1 = 100
+let score2 = 200
+let socre3 = 300
+
+console.log(Array.of(score1,score2,socre3));
+
