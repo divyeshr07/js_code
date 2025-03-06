e222189bf692d6bed381fc8967a84ad8dca510fe
divyeshr07 <divyesh.rushkar@gmail.com>
2025-03-06 12:48:41 +0000

    strings

---
+++ 01_basics/06_string.js
@@ -0,0 +1,33 @@
+const name = "divyesh"
+const sname = "nakum"
+
+// console.log(name+sname);
+
+// console.log(`my name is ${name} and my sarname ${sname}`);
+
+const projectName = new String('divyeshjs-Html')
+
+// console.log(projectName[0]);
+// console.log(projectName.__proto__);
+
+// console.log(projectName.length);
+// console.log(projectName.toUpperCase());
+// console.log(projectName.charAt(6));
+// console.log(projectName.indexOf('s'))
+
+
+//  const newString = projectName.substring(0,5)
+//  console.log(newString);
+
+// const faststring = projectName.slice(-8,4)
+// console.log(faststring);
+
+
+const oneString = "    hello      "
+console.log(oneString.trim());
+
+const url = "https;//ramesg20*gmail-com"
+
+console.log(url.replace('20*','-'));
+console.log(url.includes('ramesg'));
+console.log(projectName.split('-'));
