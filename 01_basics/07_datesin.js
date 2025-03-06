fe488a908f89d09a3375459ce16894764522cdfa
divyeshr07 <divyesh.rushkar@gmail.com>
2025-03-06 10:34:33 +0000

    datesin

---
+++ 01_basics/07_datesin.js
@@ -0,0 +1,20 @@
+//dates
+let myDate = new Date()
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toString());
+// console.log(typeof myDate);
+
+// let myBrithdayDate = new Date(2025,9,21)
+// let myBrithdayDate = new Date(2025,9,21,7,7)
+let myBrithdayDate = new Date("07-07-2004")
+// console.log(myBrithdayDate.toDateString());
+// console.log(myBrithdayDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
+console.log(myBrithdayDate.getTime());
+console.log(Date.now());
+
+
