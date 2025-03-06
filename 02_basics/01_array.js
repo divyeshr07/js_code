b24926acef16f099e30223c3f25a03551da9bd3a
divyeshr07 <divyesh.rushkar@gmail.com>
2025-03-06 12:15:13 +0000

    array1

---
+++ 02_basics/01_array.js
@@ -0,0 +1,37 @@
+
+const myArr = [1,2,3,4,5]
+const myFriend = ["manoj","rohan","dilip"]
+const myArr2 = new Array(1,2,3)
+
+// console.log(myArr[1]);
+
+
+// array methods
+
+// myArr.push(5)
+// myArr.pop()
+// myArr.unshift(6)
+// myArr.shift()
+
+// console.log(myArr.includes(5));
+// console.log(myArr.indexOf(6));
+
+const newArr = myArr.join()
+// console.log(typeof newArr);
+
+
+// console.log(myArr);
+
+
+//  slice,splice
+console.log("A",myArr);
+
+
+const myn1 = myArr.slice(1, 3)
+console.log(myn1);
+console.log("B",myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log(myn2);
+    
+
